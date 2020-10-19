ee6277374ff13f42a9ff20d975a9a6186c5b7352
genesisbell <bellmon90@hotmail.com>
2020-10-18 17:09:28 -0700

    docs agregado2

---
+++ docs/precache-manifest.db2344fb4b43e5bd584b251a812ec681.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6909e6b78eb16fcd01b176113a31ac32",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e6e2554f56d4fb25070",
+    "url": "/static/css/main.44ab5086.chunk.css"
+  },
+  {
+    "revision": "18772ccb525ea2d14384",
+    "url": "/static/js/2.57075067.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.57075067.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e6e2554f56d4fb25070",
+    "url": "/static/js/main.246aee46.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
